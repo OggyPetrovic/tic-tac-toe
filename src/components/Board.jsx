0b5b567fcf3d244307ed783814308f86fb2a254e
OggyPetrovic <ognjen56@gmail.com>
2020-12-01 23:30:19 +0100

    Implemented generic board rendering for any provided dimension

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Square from './Square';
 
 export default class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      dimension: props.dimension,
       squares: Array(9).fill(null),
       xIsNext: true,
     };
@@ -30,28 +33,37 @@ export default class Board extends React.Component {
     return <Square value={squares[i]} onClick={() => this.handleClick(i)} />;
   }
 
+  renderRow(offset) {
+    const { dimension } = this.state;
+    const squares = Array(dimension);
+    for (let i = 0; i < dimension; i += 1) {
+      squares[i] = this.renderSquare(i + offset);
+    }
+    return <div className="board-row">{squares}</div>;
+  }
+
   render() {
+    const { dimension } = this.state;
+
     const status = `Next player: ${this.nextPlayer()}`;
+    const rows = Array(dimension);
+    for (let i = 0; i < dimension; i += 1) {
+      rows[i] = this.renderRow(i * dimension);
+    }
 
     return (
       <div>
         <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {rows}
       </div>
     );
   }
 }
+
+Board.propTypes = {
+  dimension: PropTypes.number,
+};
+
+Board.defaultProps = {
+  dimension: 3,
+};
