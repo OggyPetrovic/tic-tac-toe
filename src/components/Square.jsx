b80e5c3ff1089880a5ecf405148d1654eba20409
OggyPetrovic <ognjen56@gmail.com>
2020-12-01 21:30:28 +0100

    Removed unnecessary function wrapping when assigning Square onClick.

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export default function Square(props) {
   const { value, onClick } = props;
   return (
-    <button type="button" className="square" onClick={() => { onClick(); }}>
+    <button type="button" className="square" onClick={onClick}>
       {value}
     </button>
   );
