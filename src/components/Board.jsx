120eb4524d0c3d577768f002dc60178f6aa8a361
oggy-petrovic <contact@oggypetrovic.dev>
2020-12-08 22:27:31 +0100

    Implemented victory detection.

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,12 +3,82 @@ import PropTypes from 'prop-types';
 
 import Square from './Square';
 
+function checkLine(squares, player, index, dimension, step) {
+  if (!player) {
+    return null;
+  }
+  const start = index + step;
+  const limit = dimension * step + index;
+  for (let i = start; i < limit; i += step) {
+    const element = squares[i];
+    if (element !== player) {
+      return null;
+    }
+  }
+
+  return player;
+}
+
+function checkHorizontal(squares, index, dimension) {
+  const player = squares[index];
+  const step = 1;
+  return checkLine(squares, player, index, dimension, step);
+}
+
+function checkVertical(squares, index, dimension) {
+  const player = squares[index];
+  const step = dimension;
+  return checkLine(squares, player, index, dimension, step);
+}
+
+function checkDiagonalRight(squares, index, dimension) {
+  const player = squares[index];
+  const step = dimension + 1;
+  return checkLine(squares, player, index, dimension, step);
+}
+
+function checkDiagonalLeft(squares, index, dimension) {
+  const player = squares[index];
+  const step = dimension - 1;
+  return checkLine(squares, player, index, dimension, step);
+}
+
+function findWinner(squares, dimension) {
+  let winner = null;
+  // check verticals
+  for (let i = 0; i < dimension; i += 1) {
+    winner = checkVertical(squares, i, dimension);
+    if (winner) {
+      return winner;
+    }
+  }
+  // check horizontals
+  for (let i = 0; i < dimension * dimension; i += dimension) {
+    winner = checkHorizontal(squares, i, dimension);
+    if (winner) {
+      return winner;
+    }
+  }
+  // check diagonals
+  winner = checkDiagonalRight(squares, 0, dimension);
+  if (winner) {
+    return winner;
+  }
+  winner = checkDiagonalLeft(squares, dimension - 1, dimension);
+  if (winner) {
+    return winner;
+  }
+
+  return winner;
+}
+
 export default class Board extends React.Component {
   constructor(props) {
     super(props);
+    const { dimension } = props;
     this.state = {
-      dimension: props.dimension,
-      squares: Array(9).fill(null),
+      dimension,
+      squares: Array(dimension * dimension).fill(null),
       xIsNext: true,
     };
   }
@@ -43,9 +113,13 @@ export default class Board extends React.Component {
   }
 
   render() {
-    const { dimension } = this.state;
+    const { dimension, squares } = this.state;
 
-    const status = `Next player: ${this.nextPlayer()}`;
+    const winner = findWinner(squares, dimension);
+    const status = winner
+      ? `The winner is: ${winner}`
+      : `Next player: ${this.nextPlayer()}`;
+
     const rows = Array(dimension);
     for (let i = 0; i < dimension; i += 1) {
       rows[i] = this.renderRow(i * dimension);
